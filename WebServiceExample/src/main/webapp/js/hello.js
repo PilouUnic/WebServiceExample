7a438c8ca719d256328228261f0397dc4c9f738a
aurelien.cortial <cortial_aurelien@hotmail.com>
2015-06-01 09:09:14 +0200

    Commit

--- WebServiceExample/src/main/webapp/js/hello.js
+++ WebServiceExample/src/main/webapp/js/hello.js
@@ -1,6 +1,27 @@
 function Hello($scope, $http) {
-    $http.get('http://localhost:8080/WebServiceExample/rest/hello/user').
-        success(function(data) {
-            $scope.user = data;
-        });
+	$http.get('http://localhost:8080/WebServiceExample/rest/hello/user').
+	success(function(data) {
+		$scope.user = data;
+	});
+}
+
+function myCtrl($scope) {
+	$scope.firstNameValue = "John";
+	$scope.lastNameValue = "Doe";
+}
+
+function listController($scope) {
+	$scope.names = [
+	    {name:'Jani',country:'Norway'},
+	    {name:'Hege',country:'Sweden'},
+	    {name:'Kai',country:'Denmark'}
+    ];
+}
+
+function tableController($scope) {
+	$scope.vals = [
+	    {name:'Jani',country:'Norway'},
+	    {name:'Hege',country:'Sweden'},
+	    {name:'Kai',country:'Denmark'}
+    ];
 }
